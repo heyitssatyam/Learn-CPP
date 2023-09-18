8b5d7eb7f3c3b517573ec913761d64dd41f78062
heyitssatyam <kumarsatyam2003@gmail.com>
2023-09-19 00:33:27 +0530

    Area of Traingle

---
+++ areaoftraingle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float area,b,h;
+    cout<<"Enter the Base and Height of the traingle: "<<endl;
+    cin>>b>>h;
+    area=(b*h)/2;
+    cout<<"Area of traingle is: "<<area<<endl;
+    return 0;
+}
