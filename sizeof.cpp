14970b558a6bb68dd073f0ac72822a8815349d9d
heyitssatyam <kumarsatyam2003@gmail.com>
2023-09-19 00:21:05 +0530

    sixeof program added

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char x;
+    int y;
+    float z;
+    double d;
+    cout<<sizeof(x)<<endl;
+    cout<<sizeof(y)<<endl;
+    cout<<sizeof(z)<<endl;
+    cout<<sizeof(d)<<endl;
+    return 0;
+
+}
